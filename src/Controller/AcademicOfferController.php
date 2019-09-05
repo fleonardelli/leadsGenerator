80c2fb2e04aa0c75c6137d16a14b38ed588f880e
Muc <mauro.chojrin@leewayweb.com>
2019-09-05 10:47:15 -0300

    Marcando tareas a futuro

--- src/Controller/AcademicOfferController.php
+++ src/Controller/AcademicOfferController.php
@@ -17,6 +17,8 @@ class AcademicOfferController extends AbstractController
         $academicOffers = $em->getRepository(AcademicOffer::class)
             ->findAll();
 
+        // @todo contemplar el caso de que no haya nada... Proablemente devolver un 404... Por ahora dejémoslo así pero
+        // en general, al menos vayamos tomando notas de las cosas que quedan pendientes
         return $this->json(
             [
                 'academicOffers' => $academicOffers
